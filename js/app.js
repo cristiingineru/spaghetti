6a011fd14f3ba42e17c46aed1e5ec32e0666e655
cristiingineru <cristi.ingineru@gmail.com>
2014-12-10 22:21:11 +0200

    adding React and a simple example

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-/* global requirejs,SVG  */
+/* global requirejs,SVG,React,document  */
 
 requirejs.config({
   baseUrl: 'js/lib',
@@ -7,9 +7,9 @@ requirejs.config({
   }
 });
 
-requirejs(['svg', 'svg.draggable', 'app/componentCatalog'], function (s, g, catalog) {
-  var cookerSVG = SVG('cookerPlaceholder');
-  var resistor = catalog('resistor').create();
-  var view = resistor.draw(cookerSVG);
-  resistor.move(200, 200);
+requirejs(['react'], function (React) {
+
+  var text = React.createElement('a', null, 'text');
+  React.render(text, document.getElementById('cookerPlaceholder'));
+
 });
