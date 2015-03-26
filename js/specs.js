c7f99e508ee56421ad0691477d034d922b0afbf5
cristiingineru <cristi.ingineru@gmail.com>
2015-03-26 13:00:21 +0200

    First pass of breadboardSpec.js

--- js/specs.js
+++ js/specs.js
@@ -19,6 +19,7 @@ require(
     'specs/dissectSpec',
     'specs/resistorSpec',
     'specs/capacitorSpec',
+    'specs/breadboardSpec',
     'specs/keyProviderSpec',
     'specs/coreSpec',
     'specs/layoutManagerSpec',
