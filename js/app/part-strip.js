59d9a9c311a5823d6d6f6093f59ff7f5004526a0
cristiingineru <cristi.ingineru@gmail.com>
2015-03-27 17:26:48 +0200

    Work on strip

--- js/app/part-strip.js
+++ js/app/part-strip.js
@@ -10,7 +10,27 @@ define(['React', 'immutable.min', 'app/core', 'app/part-hole'], function (React,
         model: null
       };
     },
-    render: function () {}
+    render: function () {
+      var body = React.createElement('rect', {
+        x: this.props.model.get('x'),
+        y: this.props.model.get('y'),
+        width: this.props.model.get('width'),
+        height: this.props.model.get('height'),
+        stroke: '#d4d1ca',
+        fill: '#f7eedc',
+        rx: 3,
+        ry: 3,
+        key: -1
+      });
+      var holes = this.props.model.getIn(['holes'])
+        .map(function (hole) {
+          return React.createElement(partHole.class(), {
+            model: hole,
+            key: hole.get('key')
+          });
+        }).toArray();
+      return React.createElement('g', null, [body].concat(holes));
+    }
   });
 
   var stripProto = function (model) {
@@ -21,22 +41,46 @@ define(['React', 'immutable.min', 'app/core', 'app/part-hole'], function (React,
     thisProto.setXY = function (x, y) {
       model = model.set('x', x);
       model = model.set('y', y);
-      return this.updateFinger();
+      return this.updateHoles();
     };
     thisProto.setOrientation = function (orientation) {
       model = model.set('orientation', orientation);
-      return this.updateFinger();
+      return this.updateStrip();
     };
     thisProto.setHoleCount = function (holeCount) {
       model = model.set('holeCount', holeCount);
-      return this;
+      return this.updateStrip();
     };
     thisProto.keyify = function (keyProvider) {
       model = model.set('key', keyProvider());
+      var holes = model.get('holes').map(function (hole) {
+        return hole.objectify()
+          .keyify(keyProvider)
+          .model();
+      });
+      model = model.set('holes', holes);
+      return this.updateStrip();
+    };
+    thisProto.updateStrip = function () {
+      var orientation = model.get('orientation'),
+        holeCount = model.get('holeCount'),
+        x = model.get('x'),
+        y = model.get('y'),
+        holeSize = 10;
+
+      if (orientation === 'horizontal') {
+        model = model.set('width', holeCount * holeSize)
+          .set('height', holeSize);
+      } else {
+        model = model.set('height', holeCount * holeSize)
+          .set('height', holeSize);
+      }
+
+      for (var i = 0; i < holeCount; i++) {}
       return this;
     };
     thisProto.init = function () {
-      return this;
+      return this.updateStrip();
     };
     return thisProto;
   };
@@ -48,6 +92,7 @@ define(['React', 'immutable.min', 'app/core', 'app/part-hole'], function (React,
     height: 0,
     orientation: 'horizontal',
     holeCount: 5,
+    holes: [],
     proto: stripProto
   });
 
--- js/specs/stripSpec.js
+++ js/specs/stripSpec.js
@@ -1,7 +1,7 @@
 /* global define, require, describe, it, xit, expect, dissect, update, updateAll, filter, where, spyOn, jasmine */
 
 
-define(['app/part-strip', 'Squire', 'immutable.min', 'app/layoutManager', 'React'], function (Strip, Squire, Immutable, LayoutManager, React) {
+define(['app/part-strip', 'Squire', 'immutable.min', 'app/layoutManager', 'React', 'app/keyProvider', 'app/part-hole'], function (Strip, Squire, Immutable, LayoutManager, React, KeyProvider, Hole) {
   describe('Strip', function () {
     it('should provide a known API', function () {
       expect(typeof (Strip.class)).toBe('function');
@@ -15,11 +15,65 @@ define(['app/part-strip', 'Squire', 'immutable.min', 'app/layoutManager', 'React
     });
 
     describe('Strip class', function () {
-      it('should contain hols', function () {});
+
+      var TestUtils = React.addons.TestUtils;
+      var renderStrip = function (orientation, holeCount) {
+        var model = Strip.model().objectify()
+          .keyify(KeyProvider)
+          .setOrientation(orientation)
+          .setHoleCount()
+          .model(),
+          strip = React.createElement(Strip.class(), {
+            model: model
+          });
+        return TestUtils.renderIntoDocument(strip);
+      };
+
+      it('should contain holes', function () {
+        var strip = renderStrip();
+        var holes = TestUtils.scryRenderedComponentsWithType(strip, Hole.class());
+        expect(holes.length).toBeGreaterThan(0);
+      });
     });
 
     describe('Strip proto', function () {
+      it('should set a key to itself and to its child parts', function () {
+        var hardCodedUniqueKey = 9999,
+          keyProvider = jasmine.createSpy('keyProvider').and.returnValue(hardCodedUniqueKey);
 
+        var model = Strip.model().objectify()
+          .keyify(keyProvider)
+          .model();
+
+        expect(keyProvider).toHaveBeenCalled();
+        expect(model.get('key')).toBe(hardCodedUniqueKey);
+      });
+
+      it('should adapt the size acordingly to the hole count', function () {
+        var holeCounts = [1, 2, 5];
+
+        holeCounts.forEach(function (holeCount) {
+          var model = Strip.model().objectify()
+            .setOrientation('horizontal')
+            .setHoleCount(holeCount)
+            .model();
+
+          expect(model.get('holeCount')).toBe(holeCount);
+        });
+      });
+
+      it('should change the sizes accordingly to orientation', function () {
+        var orientations = ['horizontal', 'vertical'];
+
+        orientations.forEach(function (orientation) {
+          var model = Strip.model().objectify()
+            .setOrientation('horizontal')
+            .setHoleCount(3)
+            .model();
+
+          expect(model.get('orientation')).toBe(orientation);
+        });
+      });
     });
 
     describe('Strip model', function () {
@@ -28,12 +82,14 @@ define(['app/part-strip', 'Squire', 'immutable.min', 'app/layoutManager', 'React
         expect(Immutable.Map.isMap(model)).toBe(true);
       });
 
-      var properties = ['proto', 'x', 'y', 'width', 'height', 'holeCount', 'orientation'];
+      var properties = ['x', 'y', 'width', 'height', 'holeCount', 'orientation', 'holes', 'proto'];
 
       it('should have ' + properties + ' properties', function () {
         var model = Strip.model();
         properties.forEach(function (property) {
-          expect(model.get(property)).toBe(jasmine.anything());
+          var value = model.get(property),
+            condition = value !== null && value !== undefined;
+          expect(condition).toBe(true);
         });
       });
     });
