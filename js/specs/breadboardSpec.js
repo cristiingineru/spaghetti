30fbe95cb48f0b254d96f90058f5ac8a5a8be0e6
cristiingineru <cristi.ingineru@gmail.com>
2015-03-30 21:06:48 +0300

    Cleanup

--- js/specs/breadboardSpec.js
+++ js/specs/breadboardSpec.js
@@ -1,7 +1,7 @@
 /* global define, require, describe, it, xit, expect, dissect, update, updateAll, filter, where, spyOn, jasmine */
 
 
-define(['app/component-breadboard', 'Squire', 'immutable.min', 'app/layoutManager', 'React', 'app/part-leg', 'app/part-body', 'mocks/bodyMock'], function (Breadboard, Squire, Immutable, LayoutManager, React, Leg, Body, BodyMock) {
+define(['app/component-breadboard', 'Squire', 'immutable.min', 'app/layoutManager', 'React', 'app/part-strip', 'app/keyProvider'], function (Breadboard, Squire, Immutable, LayoutManager, React, Strip, KeyProvider) {
   describe('Breadboard', function () {
     it('should provide a known API', function () {
       expect(typeof (Breadboard.name)).toBe('function');
@@ -19,40 +19,27 @@ define(['app/component-breadboard', 'Squire', 'immutable.min', 'app/layoutManage
 
     describe('Breadboard class', function () {
 
+      var TestUtils = React.addons.TestUtils;
+      var renderBreadboard = function () {
+        var model = Breadboard.model().objectify()
+          .keyify(KeyProvider)
+          .model(),
+          breadboard = React.createElement(Breadboard.class(), {
+            model: model
+          });
+        return TestUtils.renderIntoDocument(breadboard);
+      };
+
+      it('should render strips', function () {
+        var breadboard = renderBreadboard();
+        var strips = TestUtils.scryRenderedComponentsWithType(breadboard, Strip.class());
+        expect(strips.length).toBeGreaterThan(0);
+      });
     });
 
     describe('Breadboard proto', function () {
-      it('should have a keyify function that set a unique key to itself and child parts', function () {
-        var hardCodedUniqueKey = 9999;
-        var keyProvider = jasmine.createSpy('keyProvider').and.returnValue(hardCodedUniqueKey);
-        var breadboard = Breadboard.model().objectify()
-          .keyify(keyProvider)
-          .model();
-        expect(keyProvider.calls.count()).toBeGreaterThan(3);
-      [
-        breadboard.get('key'),
-        //resistor.getIn(['body', 'key']),
-        //resistor.getIn(['legs', '0', 'key']),
-        //resistor.getIn(['legs', '1', 'key'])
-      ].forEach(function (key) {
-          expect(key).toBe(hardCodedUniqueKey);
-        });
-      });
-
-      it('should have an init function that personalize the child parts', function (done) {
-        var squire = new Squire()
-          .mock('app/part-body', BodyMock)
-          .require(['app/component-resistor'], function (Resistor2) {
-            var resistor = Resistor2.model().objectify()
-              .init()
-              .model();
-            //var bodyProto = resistor.get('body').objectify();
-            //expect(bodyProto.setXY).toHaveBeenCalled();
-            //expect(bodyProto.setWidth).toHaveBeenCalled();
-            //expect(bodyProto.setHeight).toHaveBeenCalled();
-            done();
-          });
-      });
+      xit('should have a keyify function that set a unique key to itself and child parts', function () {});
+      xit('should have an init function that personalize the child parts', function (done) {});
     });
 
     describe('Breadboard model', function () {
